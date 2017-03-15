c14a017da90c5053fd79f2651106f74be63abe06
Konstantin Semenov <kvsemenoff@mail.ru>
2017-03-15 08:18:55 +0300

    js to footer

--- includes/header.php
+++ includes/header.php
@@ -9,7 +9,6 @@
 	<link href="//netdna.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.css" rel="stylesheet">
 	<link rel="stylesheet" href="libs/fancybox/jquery.fancybox.css" />
 	<link rel="stylesheet" href="libs/owl.carousel/assets/owl.carousel.css" />
-	<link rel="stylesheet" href="libs/wow/animate.css">
 	<link type="text/css" href="css/jquery.jscrollpane.css" rel="stylesheet" media="all" />
 
 	<link rel="shortcut icon" href="favicon.ico" />
@@ -17,19 +16,6 @@
 
 </head>
 <body>
-	<script src="libs/jquery/jquery-1.11.1.min.js"></script>
-	<script src="libs/owl.carousel/owl.carousel.js"></script>
-	<script src="libs/fancybox/jquery.fancybox.pack.js"></script>
-	<script src="libs/wow/wow.min.js"></script>
-	<script type="text/javascript" src="js/jquery.mousewheel.js"></script>
-	<script type="text/javascript" src="js/jquery.jscrollpane.min.js"></script>
-
-	<script>
-    	new WOW().init();
-    </script>
-
-	<script src="js/responsiveTabs.js"></script>
-	<script src="js/jquery.maskedinput.min.js"></script>
-	<script src="js/common.js"></script>
+	
 
 	
