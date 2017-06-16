175dae941114bb5340d9a8ca7f4eae89d2e57b5e
kvsemenoff <kvsemenoff@mail.ru>
2017-06-16 12:48:22 +0400

    d

--- includes/popup.php
+++ includes/popup.php
@@ -10,3 +10,12 @@
 	<br>
 	<br>gergeg
 </div>
+
+<div class="modal-window" id="thanks">
+	<span class="modal-close">Закрыть</span>
+	<br>
+	<span class="popup-title">Спасибо за вашу заявку!</span>
+	<p>
+		В ближайшее время наш менеджер <br> свяжется с Вами.
+	</p>	
+</div>
